c35651f75aba93a5acbacf9f537524447bb53b3b
Wallyson Galvão <wallyson.galvao@gmail.com>
2020-09-07 21:38:53 -0300

    first commit

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { IState } from "../../store";
+import { ICartItem } from "../../store/modules/cart/types";
+
+const Cart: React.FC = () => {
+  const cart = useSelector<IState, ICartItem[]>((state) => state.cart.items);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cart.map(({ product, quantity }) => (
+          <tr key={product.id}>
+            <td>{product.title}</td>
+            <td>{product.price}</td>
+            <td>{quantity}</td>
+            <td>{(product.price * quantity).toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Cart;
